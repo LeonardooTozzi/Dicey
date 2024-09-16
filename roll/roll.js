84242f99fac3d5137cc2d10c2e763add50a6323f
Tozzi <TozzileoDev@gmail.com>
2024-09-15 21:51:57 -0300

    Initial Commit

---
+++ roll/roll.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+app.use(express.json());
+
+app.post('/four', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
+
+app.post('/six', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
+
+app.post('/eight', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
+
+app.post('/ten', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
+
+app.post('/twelve', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
+
+app.post('/twenty', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
+
+app.post('/custom', (req, res) => {
+
+    console.log('post')
+
+    let roll = "ALOU"
+
+    res.json({ roll });
+
+});
