21d9110e38cade243327629590a3cfd1283ba21c
Tozzi <TozzileoDev@gmail.com>
2024-10-13 20:39:14 -0300

    Back end Finished

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 3000;
 app.use(express.json());
 app.use('/', require('./roll/dice'));
 
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send(err.message);
+});
+
 app.listen(port, () => {
-  console.log(`Server is running`);
+  console.log(`Dicey is running`);
 });
